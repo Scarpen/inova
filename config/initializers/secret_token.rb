9088ab18643341af617cca5ace21966ec55f98b6
rodrigo <rodrigoandradebcc@gmail.com>
2014-05-16 10:44:13 -0300

    Criacao do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PlataformaInovatech::Application.config.secret_key_base = '59a61ef31302a69af5b968ef1cb283deb4a5b1e224d5bc93c012f4dc9e331e17f455b8dbc45579463633ea1809be83318703f5d5230da21b1afe72a13f0306e5'
